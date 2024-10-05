9312fc6e5627e4f2ae6d27600b2b3951855e3b67
Dinh Nguyen Khac <khacdinh09t4@gmail.com>
2024-10-05 19:03:40 +0700

    Add screenshot

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a27e89e27983b4a74ac0b33969a7103e-642695595.us-west-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
